6ae35987a2382cedc93c4426b063de366a1a0b90
Eric Ren <rene@ren.email>
2017-07-26 14:30:08 -0700

    06 avoid object mutation

--- sredux.jsx
+++ sredux.jsx
@@ -109,6 +109,8 @@ store.subscribe(render);
 
 render();
 
+
+
 // tests
 const incrementCounter = (list, index) => {
   return [
@@ -132,3 +134,39 @@ const testIncrementCounter = () => {
 testIncrementCounter();
 
 
+
+
+
+// Todos
+const toggleTodo = (todo) => {
+  // return Object.assign({}, todo, {
+  //   completed: !todo.completed
+  // });
+  return {
+    ...todo,
+    completed: !todo.completed
+  }
+};
+
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
+console.log('All tests passed.');
